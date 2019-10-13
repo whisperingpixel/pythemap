f2b8392b44ff6a6bce6e7034ba1f627c3d802b5d
Martin Sudmanns <whisperingpixel@users.noreply.github.com>
2019-10-13 11:00:16 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(name='pythemap',
       version='0.1',
-      description='Efficient encoding of categorical geodata using prime numbers',
+      description='Investigating Schema-Free Encoding of Categorical Data Using Prime Numbers in a Geospatial Context: https://www.mdpi.com/2220-9964/8/10/453/htm',
       url='https://github.com/whisperingpixel/pythemap',
       author='Martin Sudmanns',
       author_email='martin.sudmanns@sbg.ac.at',
